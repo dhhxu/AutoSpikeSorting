39cc57bee0ebd89fa8e92da24b0004855af830e4
Daniel Xu <dhhxu88@gmail.com>
2015-07-01 12:09:39 -0400

    complete coloring pca cluster code

--- visual/plotPca2d.m
+++ visual/plotPca2d.m
@@ -1,13 +1,17 @@
-function [] = plotPca2d(score)
+function [] = plotPca2d(score, varargin)
 % PLOTPCA2D Scatter plot the first two principal components
 %
-% PLOTPCA2D(SCORE)
+% PLOTPCA2D(SCORE) plots the first two PCs in 2D space. The rows of SCORE are
+% the PCA representation of spike waveforms, and the columns correspond to
+% principal components, of which the first two are used to plot points.
 %
-% Given the PCA score matrix SCORE, scatter plot the first two principal
-% components.
+% PLOTPCA2D(SCORE, CLASS) colors the points based on the CLASS vector assigning
+% each point to some class number. This creates cluster coloring.
 %
 % INPUT:
 % SCORE     MxN numeric score matrix. N must be at least 2
+% CLASS     Mx1 integer vector. The integers represent classes, and each
+%           observation is assigned one.
 %
 % OUTPUT:
 % NONE
@@ -21,11 +25,24 @@ function [] = plotPca2d(score)
         error('Number of columns must be at least 2: %d', nCols);
     end
     
+    if nargin > 2
+        error('Too many arguments: %d', nargin);
+    elseif nargin == 2
+        if length(varargin{1}) ~= length(score)
+            error('Classification vector has wrong dimensions: %d', ...
+                length(varargin{1}));
+        end
+    end
+    
     pc1 = score(:, 1);
     pc2 = score(:, 2);
     
     opengl software;
     figure('Name', 'PCA 2D');
-    scatter(pc1, pc2, 0.9);
+    if isempty(varargin{1})
+        scatter(pc1, pc2, 0.9);
+    else
+        gscatter(pc1, pc2, varargin{1});
+    end
     
 end
