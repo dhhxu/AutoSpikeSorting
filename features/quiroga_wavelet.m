628f9cc2a611f153af69ff80fda77b2bb1942e36
Daniel Xu <dhhxu88@gmail.com>
2015-07-01 18:06:54 -0400

    add Quiroga 2004 feature extraction method

--- features/quiroga_wavelet.m
+++ features/quiroga_wavelet.m
@@ -1,151 +1,79 @@
-%% Summary
-% This script, given a block number and a channel number, finds the
-% timestamps of spike occurrences and fetches the corresponding spike
-% data in the stream. The window is about 2-3 ms.
-% Next, the fetched spikes have WT applied on them. Then the 10 best
-% wavelet coefficients are selected as per Quiroga 2004.
+function [wavelet_coeffs] = quiroga_wavelet(spike_matrix)
+% QUIROGA_WAVELET wavelet transform of spikes via Quiroga's method (2004).
+%
+% WAVELET_COEFFS = QUIROGA_WAVELET(SPIKE_MATRIX)
+%
+% Implementation of Quiroga's feature extraction method of extracting wavelet
+% coefficients via the Lilliefors modification of the Kolmogorov-Smirnov (KS)
+% test. Applies this feature extraction method to the matrix of spike waveforms,
+% SPIKE_MATRIX, where each row is a spike waveform, and the columns are waveform
+% samples. Returns a matrix with the same number of rows as SPIKE_MATRIX and 10
+% columns for the wavelet coefficients, as described in Quiroga 2004.
+%
+% Requires the Wavelet Toolbox.
+%
+% INPUT:
+% SPIKE_MATRIX      MxN numeric matrix of spike waveforms
+% 
+% OUTPUT:
+% WAVELET_COEFFS    Mx10 numeric matrix of KS test selected wavelet transform
+%                   coefficients.
+%
+% Quiroga, R. Quian, Zoltan Nadasdy, and Yoram Ben-Shaul.
+% "Unsupervised spike detection and sorting with wavelets and superparamagnetic
+% clustering." Neural computation 16.8 (2004): 1661-1687.
 
-%% Globals & Setup
-% Load other scripts
-% Important one is TDT2mat.m
-opengl software
-
-if load_path()
-    error('Failed to load path!');
-end
-
-ROOT = fullfile('E:', 'AutoMaticSorter');
-
-TANK_NAME = 'AOS002';
-BLOCK = 1;
-CHANNEL = 1;
-
-addpath(genpath(ROOT));
-
-%% Load data.
-
-[status, data] = load_simple(TANK_NAME, BLOCK, ROOT);
-
-if status
-    error('Error in load_simple. Check warning messages.');
-end
-
-%% Get data structs.
-
-strm_struct = data.streams.STRM;
-snip_struct = data.snips.CSPK;
-
-%% isolate channel, get other information.
-
-strm_data = strm_struct.data(CHANNEL, :);
-strm_fs = strm_struct.fs;                       % Sample rate (Hz)
-
-snip_chan_loc = snip_struct.chan == CHANNEL;
-snip_data = snip_struct.data(snip_chan_loc, :);
-snip_ts = snip_struct.ts(snip_chan_loc);        % Units in seconds
-
-clear snip_chan_loc
-
-%% Associate snippet timestamp to stream data
-
-% Use discrete values.
-strm_idx = floor(snip_ts * strm_fs);
-
-% This corresponds to a
-% roughly 2.7 ms window size for the given sampling rate.
-WINDOW = 32;
-
-%% build the matrix of spike data in BLOCK and CHANNEL.
-
-spike_matrix = get_spikes(strm_data, strm_idx, 32);
-
-%% Wavelet decomposition (Quiroga 2004)
-
-n = size(spike_matrix, 1);
-s = size(spike_matrix, 2);
-
-wavelet_coeff = zeros(n, s);
-l_matrix = [];
-
-LVL = 4;
-WAVELET = 'haar';
-
-for i = 1:length(spike_matrix)
-    [c, l] = wavedec(spike_matrix(i, :), LVL, WAVELET);
-    wavelet_coeff(i, 1:s) = c(1:s);
-    l_matrix = vertcat(l_matrix, l); %#ok<AGROW>
-    clear c l
-end
-
-
-%% Coefficent selection via modified KS test (Lillefors).
-% Source: Wave_clus (Quiroga)
-
-NUM_COEFF = 10;
-
-for i = 1:s
-    col = wavelet_coeff(:, i);
-    thr = 3 * std(col);
-    thr_min = mean(col) - thr;
-    thr_max = mean(col) + thr;
-    
-    valid = wavelet_coeff((col > thr_min & col < thr_max), i);
-    if length(valid) > NUM_COEFF;
-        [ksstat] = test_ks(valid);
-        [~, ~, k2] = lillietest(valid);
-        sd(i) = ksstat; %#ok<*SAGROW>
-        sd2(i) = k2;
-    else
-        sd(i) = 0;
-        sd2(i) = 0;
+    if ~exist('wavedec', 'file')
+        error('Missing Wavelet toolbox');
     end
-    clear thr_min thr_max thr col
+
+    if isempty(spike_matrix)
+        error('Invalid spike matrix');
+    end
+    
+    LEVEL = 4;
+    WAVELET = 'haar';
+    NUM_COEFF = 10;
+    
+    [n, p] = size(spike_matrix);
+    
+    coeff_matrix = zeros(n, p);
+    
+    for i = 1:n
+        [C, ~] = wavedec(spike_matrix(i, :), LEVEL, WAVELET);
+        coeff_matrix(i, 1:p) = C(1:p);
+    end
+    
+    best_coeff_indices = select_coeffs(coeff_matrix, NUM_COEFF);
+    
+    wavelet_coeffs = coeff_matrix(:, best_coeff_indices);
+
 end
 
-[~, I] = sort(sd, 2, 'descend');
-[~, I2] = sort(sd2, 2, 'descend');
+function [best_coeff_indices] = select_coeffs(coeff_matrix, max_coeff)
+% Use Lilliefors modification of KS test to calculate the best MAX_COEFF
+% coefficients of COEFF_MATRIX.
 
-% Indices of the coefficients to use.
-coeffs(1:NUM_COEFF) = I(1:NUM_COEFF);
-coeffs2(1:NUM_COEFF) = I2(1:NUM_COEFF);
-
-%% plot
-
-wavelet_coeff_reduced = wavelet_coeff(:, coeffs);
-
-figure('Name', 'First 3 coefficients');
-scatter3(wavelet_coeff_reduced(:, 1), wavelet_coeff_reduced(:, 2), wavelet_coeff_reduced(:, 3), 0.9);
-% scatter(wavelet_coeff_reduced(:, 1), wavelet_coeff_reduced(:, 2), 0.9);
+    ks_values = zeros(1, size(coeff_matrix, 2));
     
+    for i = 1:size(coeff_matrix, 2)
+        column = coeff_matrix(:, i);
+        spread = 3 * std(column);
+        max_threshold = mean(column) + spread;
+        min_threshold = mean(column) - spread;
+        
+        valid_indices = column > min_threshold & column < max_threshold;
+        valid_elements = coeff_matrix(valid_indices, i);
+        
+        if length(valid_elements) > max_coeff
+            [~, ~, kstat] = lillietest(valid_elements);
+            ks_values(i) = kstat;
+        else
+            ks_values(i) = 0;
+        end
+    end
     
-%% compare to PCA
+    [~, ordered_coeff_indices] = sort(ks_values, 2, 'descend');
+    best_coeff_indices = ordered_coeff_indices(1:10);
 
-[C, S, L] = pca(spike_matrix);
-
-pca_coord = S(:, 1:3);
-
-figure('Name', 'PCA');
-% scatter3(pca_coord(:, 1), pca_coord(:, 2), pca_coord(:,3), 0.9);
-scatter(pca_coord(:, 1), pca_coord(:, 2), 0.9);
-
-%% PCA on aligned stream data
-
-[Ca, Sa, La] = pca(align_spikes(spike_matrix, 10, 'neg', 'n', 2));
-figure('Name', 'Aligned PCA');
-scatter(Ss(:, 1), Ss(:, 2), 0.9);
-
-
-%% PCA on snippet data
-
-[Cs, Ss, Ls] = pca((snip_data));
-
-figure('Name', 'Snip PCA');
-scatter(Ss(:, 1), Ss(:, 2), 0.9);
-
-%% test own alignment
-
-
-plot_spikes(align_spikes(spike_matrix, 10, 'neg', 'n', 2));
-
-%%
-plot_spikes(align_simple(spike_matrix, 10, 'min'));
+end
