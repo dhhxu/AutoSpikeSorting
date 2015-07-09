ca5ee95c9dc2084c5e132e89f8fe172463c20f02
Daniel Xu <dhhxu88@gmail.com>
2015-07-09 17:54:38 -0400

    add dimension parameter

--- visual/pca_visual.m
+++ visual/pca_visual.m
@@ -1,19 +1,33 @@
-function [] = pca_visual(spike_matrix)
-% PCA_VISUAL Visualize the first 3 principal components to assist in determining
+function [] = pca_visual(spike_matrix, dim)
+% PCA_VISUAL Visualize the principal components to assist in determining
 % the number of clusters by hand.
 %
-% PCA_VISUAL(SPIKE_MATRIX)
+% PCA_VISUAL(SPIKE_MATRIX, DIM)
 %
 % Given spike waveform data SPIKE_MATRIX, where rows are spikes and columns are
-% samples, plot the first three principal components (via PCA) in 3D space. This
-% aims to assist in 'eyeballing' the data to determine the number of clusters
-% present, which is required for some clustering algorithms such as k-means.
+% samples, plot the first three principal components (via PCA) in DIM-D space.
+% This aims to assist in 'eyeballing' the data to determine the number of
+% clusters present, which is required for some clustering algorithms such as
+% k-means
+%
+% INPUT:
+% SPIKE_MATRIX  NxM matrix, rows are spikes
+% DIM           Integer of number of dimensions to plot. Either 2 or 3.
+% 
+% OUTPUT:
+% NONE
 
     if isempty(spike_matrix)
         error('Invalid spike matrix');
     end
     
     pca_info = pca_apply(spike_matrix);
-    plotPca3d(pca_info.score);
+    
+    if dim == 2 || dim == 3
+        cmd = sprintf('plotPca%dd(pca_info.score)', dim);
+        eval(cmd);
+    else
+        error('Dim must be 2 or 3');
+    end
 
 end
