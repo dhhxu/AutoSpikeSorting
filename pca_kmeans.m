3b7028f70b7821546d343b9a14123c8da6c5e388
Daniel Xu <dhhxu88@gmail.com>
2015-07-09 17:53:51 -0400

    now parametrized to take in struct of variables computed by initialize.m

--- pca_kmeans.m
+++ pca_kmeans.m
@@ -1,25 +1,36 @@
-function pca_kmeans
+function class = pca_kmeans(info)
 % PCA_KMEANS baseline clustering procedure against which novel procedures are
 % evaluated.
+%
+% CLASS = PCA_KMEANS(INFO)
+%
+% Baseline K-means clustering using PCA for features. Takes in spike information
+% described in the INFO struct and outputs a CLASS vector of integer labels for
+% each spike.
 %
 % Filter: bandpass filter with elliptic passband (300-3000 Hz)
 % Detection: determined from TDT timestamps
 % Extraction: Symmetric window of 32 samples corresponding to ~1.3 ms
 % Features: PCA with coefficents corresponding to at least 80% variance
 % Clustering: k-means, K must be specified by user
-
+%
+% INPUT:
+% INFO      Struct containing spike data
+%
+% OUTPUT:
+% CLASS     1-D vector of integer class labels
 
 %% Step 1: Filtering
 
 %% Step 2: Spike extraction and alignment
 
-%% Step 4: Feature Extraction
+%% Step 3: Feature Extraction
 
-    features = pca_coeff(.8, size(SPIKE_MATRIX, 2), SPIKE_MATRIX);
+    features = pca_coeff(.8, size(info.SPIKE_MATRIX, 2), info.SPIKE_MATRIX);
 
-%% Step 5: Clustering
+%% Step 4: Clustering
 
-    nc = preview_clusters(SPIKE_MATRIX);
+    nc = preview_clusters(info.SPIKE_MATRIX);
 
     if nc
         class = kmeans(features, nc);
@@ -27,18 +38,14 @@ function pca_kmeans
         error('Invalid estimate entered');
     end
 
-%% Step 6: Evaluation
+%% Step 5: Evaluation
 % Put code for evaluating the quality of the clustering step.
 
-    clusters = separate_clusters(spike_matrix, class);
+   draw_clusters(info.SPIKE_MATRIX, class);
 
-    for i = 1:nc
-        plot_spikes_with_mean(clusters{i}, 'r');
-    end
+%% Step 6: Additional Processing (if necessary)
 
-%% Step 7: Additional Processing (if necessary)
-
-%% Step 8: Visualization (optional)
+%% Step 7: Visualization (optional)
 % Please add the line 'opengl software' to your visualization scripts.
 
     plotPca2d(features, class);
