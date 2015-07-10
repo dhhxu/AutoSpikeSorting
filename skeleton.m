e1a5286fa1a2afb1bb8edd60601e5bbcf725ecd3
Daniel Xu <dhhxu88@gmail.com>
2015-07-10 15:32:09 -0400

    refactor skeleton to be template for sorting algorithm

--- skeleton.m
+++ skeleton.m
@@ -1,145 +1,49 @@
-%% Shell for automated spike sorting algorithms
-% The purpose of this function is to provide a base platform to make using and
-% testing various methods for all the spike sorting steps easier.
-% 
-% To use this skeleton, change all occurrences of 'FILL' with the
-% appropriate values. For example, you may need to add a string or a function
-% statement. Then, save the skeleton to a descriptive file name for later
-% use/modification. The skeleton itself need not be modified otherwise.
+function class = skeleton(info)
+% SKELETON template for spike sorting algorithms
 %
-% The data is assumed to be from TDT, and the 'Load' section makes this
-% assumption.
+% CLASS = SKELETON(INFO)
 %
-% In spike sorting, we cluster spikes belonging to a particular channel. In
-% principle, the skeleton could loop over all channels in all blocks for a
-% given tank. As this may not be very fast, the skeleton is currently limited to
-% clustering spikes on a single channel basis. However, it can be extended to
-% handle other channels/blocks if so desired.
+% Template for spike sorting algorithms. INFO contains spike waveform data and
+% other variables of use. Default processing of the data is described below:
 %
-% Note that the given steps are merely guidelines. Depending on the procedure,
-% some steps may be unused or be invoked in a different order. Furthermore, the
-% comments in each step are suggested ways of implementing that particular step.
-% Please modify them to suit your particular needs.
+% Filter: bandpass filter with elliptic passband (300-3000 Hz)
+% Detection: determined from TDT timestamps
+% Extraction: Symmetric window of 32 samples corresponding to ~2.6 ms @~12.2 kHz
 %
-% The ideal usage case would be a single function for all steps (excluding Steps
-% 0 and 7), as this implies consistency in the output format across different
-% functions for each step and fits the 'plug and play' intentions of this
-% skeleton.
-
-function skeleton
-
-%% Step 0: Load paths and data
-% Loads path, requisite for external scripts. Provide the ROOT directory of the
-% project, which should contain all the files you will be working with,
-% including data files. Also fill in your TANK name and BLOCK number here.
-
-ROOT = 'FILL';
-TANK = 'FILL';
-BLOCK = 'FILL';
-
-addpath(genpath(ROOT));
-
-if load_path()
-    error('Failed to load paths');
-end
-
-[status, data] = load_simple(TANK, BLOCK, ROOT);
-
-if status
-    clear status
-    error('Error in load_simple. Check warning messages.');
-end
-clear status
-
-strm_data = data.streams.STRM;
-snip_data = data.snips.CSPK;
-
-FS = strm_data.fs;
-
-
-%% Step 1: Filtering and preprocessing
-% You should implement your filter and/or preprocessing methods as functions for
-% ease of usage in this skeleton.
-% Functions should generally be in the form:
+% The algorithm may implement a different way of the above steps. Please refer
+% to the 'initialize.m' script documentation for more details.z
 %
-%   processed_data = FUNCTION(data, ...)
+% Usage: save this file to a new function and give it a descriptive name.
 %
-% where DATA is raw spike voltage data and '...' signifies additional arguments
-% specific to the function used. PROCESSED_DATA generally should be a one
-% dimensional vector of the processed raw data.
-
-% FILL
-% processed_data = FUNCTION(data, ...);
-
-%% Step 2: Spike detection
-% You should implement your detection method as a function for ease of usage in
-% this skeleton.
-% The function should generally be in the form:
+% Features: FILL IN
+% Clustering: FILL IN
 %
-%   spike_indices = FUNCTION(processed_data, ...);
+% INPUT:
+% INFO      Struct containing spike data
 %
-% where PROCESSED_DATA is the processed data from Step 1 and '...' signifies
-% additional arguments specific to the function used. SPIKE_INDICES generally
-% should be a one dimensional vector of indices of detected spike occurrences.
-
-% FILL
-% spike_indices = FUNCTION(processed_data, ...);
-
-
-%% Step 3: Spike recovery
-% You should implement your recovery method as a function for ease of usage in
-% this skeleton.
-% The function should generally be in the form:
+% OUTPUT:
+% CLASS     1-D vector of integer class labels
 %
-%   spike_matrix = FUNCTION(spike_indices, ...);
-%
-% where SPIKE_INDICES is the vector of indices representing spike occurrences
-% from Step 2 and '...' signifies additional arguments specific to the function
-% used. SPIKE_MATRIX is generally a MxN matrix where M is the number of
-% recovered spike waveforms and N is the number of samples in a waveform.
+% See also INITIALIZE.
 
-% FILL
-% spike_matrix = FUNCTION(spike_indices, ...);
+%% Step 1: Filtering
+
+%% Step 2: Spike extraction and alignment
+
+%% Step 3: Feature Extraction
 
 
-%% Step 4: Feature Extraction
-% You should implement your feature extraction method as a function for ease of
-% usage in this skeleton.
-% The function should generally be in the form:
-%
-%   features = FUNCTION(spike_matrix, ...);
-%
-% where SPIKE_MATRIX is the MxN waveform matrix from Step 3 and '...' signifies
-% additional arguments specific to the function used. Unlike previous steps,
-% the format of the FEATURES output is up to you to define, as there are many
-% ways to represent spike features.
+%% Step 4: Clustering
 
-% FILL
-% features = FUNCTION(spike_matrix, ...);
 
-%% Step 5: Clustering
-% You should implement your clustering algorithm as a function for ease of
-% usage in this skeleton.
-% The function should generally be in the form:
-%
-%   classification = FUNCTION(features, ...);
-%
-% where FEATURES is the output derived from Step 4 and '...' signifies
-% additional arguments specific to the function used. Similar to Step 4, the
-% output CLASSIFICATION is dependent on the function and may require additional
-% processing.
-%
-% Ideally, the post-processing step could be implemented within the
-% clustering function so that CLASSIFICATION format will be consistent across
-% different clustering functions.
+%% Step 5: Evaluation
+% Put code for evaluating the quality of the clustering step.
 
-% FILL
-% classification = FUNCTION(features, ...);
 
 
 %% Step 6: Additional Processing (if necessary)
 
-
 %% Step 7: Visualization (optional)
+% Please add the line 'opengl software' to your visualization scripts.
 
-opengl software
+
