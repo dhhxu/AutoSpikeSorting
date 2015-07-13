340c81d76fdd4bbec4a5ce194f4de51f4aadf01a
Daniel Xu <dhhxu88@gmail.com>
2015-07-13 19:00:19 -0400

    change error to warning if nc too large

--- quality/isolation_distance.m
+++ quality/isolation_distance.m
@@ -8,9 +8,20 @@ function [iso_dist] = isolation_distance(feature_matrix, cluster_indices)
 % FEATURE_MATRIX, where rows are spikes, and the feature space is F-dimensional.
 % The cluster c is described by a vector of indices into FEATURE_MATRIX.
 %
+% Note that the number of points in any given cluster must not exceed half of
+% the total number of points. If this occurs, NaN is returned.
+%
 % The metric comes from Schmitzer-Torbert's 2005 paper. Isolation distance is
 % negatively correlated with Type I errors (false positives). High isolation
 % distance with low L-ratio indicates better cluster quality.
+%
+% INPUT:
+% FEATURE_MATRIX    NxF matrix of N waveforms with F dimensions
+% CLUSTER_INDICES   Mx1 vector of indices of the cluster to evaluate isolation
+%                   distance for.
+%
+% OUTPUT:
+% ISO_DIST          Isolation distance. If conditions do not hold, will be -1.
 %
 % Schmitzer-Torbert, N., Jackson, J., Henze, D., Harris, K., & Redish, A. D.
 % (2005). Quantitative measures of cluster quality for use in extracellular
@@ -27,12 +38,13 @@ function [iso_dist] = isolation_distance(feature_matrix, cluster_indices)
 end
 
 function dist = iso_dist_core(feature_matrix, cluster_indices)
-
+    dist = -1;
     nc = length(cluster_indices);
     others = setdiff(1:size(feature_matrix, 1), cluster_indices);
     
     if nc > length(others)
-        error('Cannot calculate isolation distance. Nc too large');
+        warning('Cannot calculate isolation distance. Nc too large');
+        return;
     end
     
     D = mahal(feature_matrix, feature_matrix(cluster_indices, :));
