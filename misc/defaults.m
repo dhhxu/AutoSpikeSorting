ad175e7b962644e4c3bd9a6eff49c09ff8e0d737
Daniel Xu <dhhxu88@gmail.com>
2015-07-15 17:15:52 -0400

    add pre and post default values

--- misc/defaults.m
+++ misc/defaults.m
@@ -8,9 +8,15 @@ function DEFAULTS = defaults()
 %
 % Fields in the struct:
 %   WINDOW      Number of samples to extract froms spike. (Default: 32)
+%   PRE         Number of samples (minus one) to extract before spike
+%               occurrence. (Default: 10)
+%   POST        Number of samples to extract after spike occurrence.
+%               (Default: 22)
 %   LO          Left cutoff frequency in Hz. (Default: 300)
 %   HI          Right cutoff frequency in Hz. (Default: 3000)
 %   SHIFT       Maximum timesteps to shift spike for alignment. (Default: 10)
+%
+% Generally either WINDOW or PRE & POST will be used at a time.
 %
 % INPUT:
 % NONE
@@ -20,6 +26,12 @@ function DEFAULTS = defaults()
 
     % Number of samples each spike waveform has. Used with symmetric windows.
     DEFAULTS.WINDOW = 32;
+    
+    % Number of samples minus one before the waveform occurrence.
+    DEFAULTS.PRE = 10;
+    
+    % Number of samples after the waveform occurrence.
+    DEFAULTS.POST = 22;
     
     % Left edge of bandpass filter in Hz.
     DEFAULTS.LO = 300;
