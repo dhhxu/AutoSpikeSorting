2aa3277a73cc67685e5361d06c348b6f5bb2fc74
Daniel Xu <dhhxu88@gmail.com>
2015-07-16 17:09:44 -0400

    change argument order

--- misc/align_snip.m
+++ misc/align_snip.m
@@ -1,11 +1,11 @@
-function aligned = align_snip(snips, thr, shift, fft)
+function aligned = align_snip(snips, shift, thr, fft)
 % ALIGN_SNIP align snippet data using normal and FFT alignment methods.
 %
-% ALIGNED = ALIGN_SNIP(SNIPS, THR, SHIFT)
+% ALIGNED = ALIGN_SNIP(SNIPS, SHIFT, THR)
 %           Amplitude alignment on maximum. THR option determines max or
 %           min. Maximum time step shift is SHIFT.
 %
-% ALIGNED = ALIGN_SNIP(SNIPS, THR, SHIFT, FFT)
+% ALIGNED = ALIGN_SNIP(SNIPS, SHIFT, THR, FFT)
 %           Applies DFT on waveforms after amplitude alignment.
 %
 % Align snippet data from TDT on maximum and using default alignment parameters.
@@ -14,15 +14,15 @@ function aligned = align_snip(snips, thr, shift, fft)
 %
 % INPUT:
 % SNIPS     Matrix of snippet data, rows are waveforms
+% SHIFT     Maximum timesteps to shift spikes for alignment.
 % THR       Option to align on maximum or miniumum. Options are:
 %               'max'
 %               'min'
-% SHIFT     Maximum timesteps to shift spikes for alignment.
-% FFT       Optional, boolean. If True, apply DFT after amplitude alignment
+% FFT       (Optional) boolean. If True, apply DFT after amplitude alignment
 %
 % OUTPUT:
 % ALIGNED   Matrix of aligned snippet spikes. Same dimensions as SNIPS. If FFT
-%           flag is set to True, this matrix is complex.
+%           flag is set to True, this matrix is complex-valued.
 
     SetDefaultValue(4, 'fft', false);
 
@@ -30,7 +30,7 @@ function aligned = align_snip(snips, thr, shift, fft)
     width = size(snips, 2);
     
     % this function checks if THR is valid.
-    aligned = align_custom(snips, shift, thr, width / 2, width /2);
+    aligned = align_custom(snips, shift, thr, width / 2, width / 2);
     
     if fft
         aligned = align_fft(snips);
