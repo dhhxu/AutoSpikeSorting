85f9e6634d89153a9db3addc0995a5ac088a8a36
Daniel Xu <dhhxu88@gmail.com>
2015-08-06 16:56:16 -0400

    complete sorter program initialization script

--- sorter.m
+++ sorter.m
@@ -1,53 +1,84 @@
 % Tank sorter script
 %
 % Perform semi-automatic spike sorting on a TDT data tank. The user supervises
-% block receptive field detection, spike alignment, and number of clusters.
+% spike alignment and number of clusters.
 %
 % Clustering is performed on the scale of receptive fields and each channel is
-% clustered individually.
+% clustered individually. Although TDT's data is on the block scale, receptive
+% fields may span several blocks and/or parts of blocks. To accommodate this,
+% "superblocks" are created from the tank data. Each superblock may contain one
+% or more blocks, and/or parts of blocks. Furthermore, a superblock will contain
+% exactly one receptive field.
 %
-% After clustering is completed for a receptive field, a Superspiketrain object
-% is constructed for each unit on a channel for all channels for that receptive
-% field. The object will be saved to a user-specified directory.
+% After clustering is completed for a channel on a superblock, a Superspiketrain
+% object is constructed for each unit identified in that channel.
+% Figures/statistics are also generated for each channel. This is repeated for
+% each channel on each superblock.
 %
-% The actual class labels will be saved on a per-block basis for future
-% validation.
+% The generated data files will be stored in a directory with the same name as
+% the tank chosen. This directory will be located in an user-specified data
+% directory.
+%
+% Author: Daniel Xu (dhhxu88@gmail.com)
+% Mentor: David Martel
 
 fprintf('\nsorter: begin\n');
 
-%% Load paths
+%% Initialization
+
+% Improve figure rendering performance.
+set(0, 'DefaultFigureRenderer', 'OpenGL');
+
+% This function assumes that SORTER.M is in the project root e.g.
+% '.../AutoSpikeSorting/'
 load_path(pwd);
 
 %% Get tank path
-[TANK, PATH] = sorter_get_tank();
+[tank, tank_path] = sorter_get_tank('U:/');
 
-if isempty(TANK) || isempty(PATH)
-    return;
-end
-
-tank_info.tank = TANK;
-tank_info.path = PATH;
-
-clear TANK PATH;
-
-%% Get data save directory
-DATA_PATH = sorter_get_dir();
-
-if isempty(DATA_PATH)
+if isempty(tank) || isempty(tank_path)
     return
 end
 
-%% Get sorting procedure
-PROC = sorter_get_procedure();
+tank_info.tank = tank;
+tank_info.path = tank_path;
+
+clear tank path;
+
+%% Get feature file
+[feat_handle] = sorter_get_feature('U:/DanielX');
+
+if isempty(feat_handle)
+    return
+end
+
+pause(1);
+
+%% Get algorithm file
+[algo_handle] = sorter_get_algorithm('U:/DanielX');
+
+if isempty(algo_handle)
+    return
+end
+
+pause(1);
+
+%% Get data save directory
+DATA_PATH = uigetdir2('', 'Select data output directory');
+
+if isempty(DATA_PATH)
+    warning('No data directory selected');
+    return
+end
 
 %% Get RF super blocks
 tic
-[superblocks, rf_idx] = build_rfblock(tank_info.path, DATA_PATH);
+[superblocks, ~, ~] = build_rfblock(tank_info.path, DATA_PATH);
 toc
+
 %% Cluster by channel, construct SST objects
-sorter_cluster_superblock(superblocks, PROC, tank_info, SST_PATH);
+sorter_cluster_superblock(superblocks, feat_handle, algo_handle, tank_info, ...
+                          DATA_PATH);
 
-%% Save clustering results on per-block basis
-
-
+%%
 fprintf('\nsorter: end\n');
