14f500612a55434e33b8a6fc35c363517bccf9c7
Daniel Xu <dhhxu88@gmail.com>
2015-08-06 16:47:11 -0400

    change documentation

--- features/diffMap.m
+++ features/diffMap.m
@@ -1,15 +1,15 @@
 function [dm] = diffMap(spikes)
-% DIFFMAP calculate diffusion map for spike waveforms.
+% DIFFMAP calculate diffusion map feature for spike waveforms.
 %
-% DM = DIFFMAP(SPIKES)
+%   DM = DIFFMAP(SPIKES)
 %
-% Calculate diffusion maps for spike matrix SPIKES using default diffusion
-% map settings (t = 1.0, sigma = 1.0). The resulting map is embedded in 3D
-% space.
+%   Calculate diffusion maps for spike matrix SPIKES using default diffusion
+%   map settings (t = 1.0, sigma = 1.0). The resulting map is embedded in 3D
+%   space.
 %
-% Note that the computation is very slow if the number of spikes is greater
-% than 3000. So for large datasets, use of this feature is strongly
-% discouraged.
+%   Note that the computation is very slow if the number of spikes is greater
+%   than 3000. So for large datasets, use of this feature is strongly
+%   discouraged.
 %
 % INPUT:
 % SPIKES    NxM matrix of spike waveform data. Rows correspond to spikes.
